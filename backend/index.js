1bc941d3f574cd8468f20ffed65cb6653361e7bd
Luis Chilo <gfxargentina@gmail.com>
2022-03-09 11:45:49 -0300

    nodejs mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(5000, () => {
+dotenv.config();
+
+const url = "mongodb://127.0.0.1/ecommerce";
+
+mongoose
+  .connect(url)
+  .then(() => console.log("DB conectada!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor corriendo OK!!");
 });
